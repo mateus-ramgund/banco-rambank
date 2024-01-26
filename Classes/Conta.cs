f369a7c84a9425d63ff0a0813be0b39aec362b88
Mateus Ramgund <120752832+mateus-ramgund@users.noreply.github.com>
2024-01-25 23:47:49 -0300

    Update Conta.cs

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -8,21 +8,24 @@ namespace banco_rambank.Classes
     public class Conta
     {
         public int AccountNumber { get; set; }
-        public string Titular { get; set; }
         protected decimal Saldo = 3000;
         private string Password { get; set; }
+        public Titular titular = new Titular();
 
         public void CreatePassword (string password)
         {
             Password = password;
         }
 
+
         public void ExibirInformacoes()
         {
             Console.WriteLine("Bem-vindo à área do cliente da RamBank.");
-            Console.WriteLine($"Titular: {Titular}");
+            Console.WriteLine($"Titular: {titular.Nome}");
             Console.WriteLine($"Número da conta: {AccountNumber}");
             Console.WriteLine($"Saldo: {Saldo}");
         }
+
+        
     }
 }
