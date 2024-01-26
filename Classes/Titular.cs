669639bd6121ac5e3eaee003cb7a09d505383351
Mateus Ramgund <120752832+mateus-ramgund@users.noreply.github.com>
2024-01-25 23:48:36 -0300

    Adicionada a class titular

---
+++ Classes/Titular.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace banco_rambank.Classes
+{
+    public class Titular
+    {
+        public string Nome { get; set; }
+    }
+}
