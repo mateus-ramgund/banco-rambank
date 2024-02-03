ce75b878a3127417cf0662526ec4382d7f86811b
Mateus Ramgund <120752832+mateus-ramgund@users.noreply.github.com>
2024-02-02 23:52:53 -0300

    Update Titular.cs

--- Classes/Titular.cs
+++ Classes/Titular.cs
@@ -7,6 +7,15 @@ namespace banco_rambank.Classes
 {
     public class Titular
     {
-        public string Nome { get; set; }
+        public Titular(string nome, string cpf, string endereco)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Endereco = endereco;
+            
+        }
+        public string Nome {get; set;}
+        public string Cpf {get; set;}
+        public string Endereco {get; set;}
     }
 }
